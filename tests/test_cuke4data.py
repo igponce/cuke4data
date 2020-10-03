b7d3556c20918448a3d6bbb69a4be37b4844fc84
Inigo <igponce@gmail.com>
2020-10-03 08:39:06 +0200

    Refactor tests.
    - Python2 removed (no __future__)
    - Test are now unittest classes
    - Added test to check rules are parsed.

--- tests/test_cuke4data.py
+++ tests/test_cuke4data.py
@@ -15,48 +15,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
+import unittest
+import pytest
 from cuke4data import gherkin as gkparser
-import pdb
 
-
-
-def test_createGherkinScenario():
-    scenarioName = 'Scenario name - string 123'
-    sc = gkparser.gherkinScenario(scenarioName)
-    assert sc.name == scenarioName
-
-def test_createGherkinRule():
-    ruleText = "Given I have a rule text like this one"
-    rr = gkparser.gherkinRule(ruleText)
-    assert rr.text == ruleText
-
-def test_parseGherkinFromFile():
-    source = open("tests/simple.gherkin", 'r')
+@pytest.fixture
+def fixture_unnamedGherkinScenario(fixture):
+    scenario_to_test = """
+        Given I have a rule like this\n"
+        And something happens\n"
+        Then do action
+    """
     gk = gkparser.gherkin()
-    gk.parse(source)
-    print("--")
-    assert gk.scenarios[0].name == '"Prose to normalize user records"'
+    gk.parse(scenario_to_test)
+    return gk
 
-def test_parse_ScenarioName_with_GherkinFromString():
-    source = """
-        Scenario: Test scenario for gherkin
+class Gherkin_Class_Tests(unittest.TestCase):
+    """
+    Testcases por individual components (classes).
+    """
 
-        When I read this content from a string
-        And I am not reading from a file
-        Then I should be able to parse the file
-        """
-   
-    gk = gkparser.gherkin()
-    gk.parse(source)
-    # Todo: Dump scenarios
-    assert gk.scenarios[0].name == "Test scenario for gherkin"
-    assert gk.scenarios[0].rules == []
+    def test_createGherkinScenario(self):
+        scenarioName = 'Scenario name - string 123'
+        sc = gkparser.gherkinScenario(scenarioName)
+        assert sc.name == scenarioName
+
+    def test_createGherkinRule(self):
+        ruleText = "Given I have a rule text like this one"
+        rr = gkparser.gherkinRule(ruleText)
+        assert rr.text == ruleText
 
 
-def test_unnamed_scenario():
-    ruleTest = "Given I have a rule like this\n" "And something happens\n" "Then do action\n"
+class Gherkin_Interface_Tests(unittest.TestCase):
 
-    gk= gkparser.gherkin()
-    gk.parse(ruleTest)
-    assert gk.scenarios[0].name == "No name"
+    def test_parseGherkinFromFile(self):
+        source = open("tests/simple.gherkin", 'r')
+        gk = gkparser.gherkin()
+        gk.parse(source)
+        print("--")
+        assert gk.scenarios[0].name == '"Prose to normalize user records"'
+
+    def test_parse_ScenarioName_with_GherkinFromString(self):
+        source = """
+            Scenario: Test scenario for gherkin
+
+            When I read this content from a string
+            And I am not reading from a file
+            Then I should be able to parse the file
+            """
+    
+        gk = gkparser.gherkin()
+        gk.parse(source)
+        # Todo: Dump scenarios
+        assert gk.scenarios[0].name == "Test scenario for gherkin"
+        assert gk.scenarios[0].rules == []
+
+    def test_unnamed_scenario(self):
+        ruleTest = "Given I have a rule like this\n" "And something happens\n" "Then do action\n"
+
+        gk= gkparser.gherkin()
+        gk.parse(ruleTest)
+        assert gk.scenarios[0].name == "No name"
+
+    def test_number_of_rules(self):
+        ruleTest = "Given I have a rule like this\n" "And something happens\n" "Then do action\n"
+        gk= gkparser.gherkin()
+        gk.parse(ruleTest)
+        assert len(gk.scenarios[0].rules)== 12123123
+
+# unittest boilerplate 
+if __name__ == "__main__":
+    unittest.main()
